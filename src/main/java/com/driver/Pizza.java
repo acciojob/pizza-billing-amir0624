63e921b05996fde22c36230b4d205ad2cebba4d9
amir0624 <mohammadamir0624@gmail.com>
2023-01-19 12:53:08 +0530

    Pizza is Ready!!

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,25 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    boolean cheeseAdded = false;
+    boolean toppingsAdded = false;
+
+    private boolean takeawayAdded = false;
+
+    private int basePrice;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg == true){
+            this.price = 300;
+
+        }
+        else {
+            this.price = 400;
+
+        }
+        basePrice = this.price;
     }
 
     public int getPrice(){
@@ -16,19 +32,57 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+
+        if(!cheeseAdded) {
+
+            cheeseAdded = true;
+            this.price += 80;
+
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+       if(!toppingsAdded) {
+            toppingsAdded = true;
+            if (isVeg == true) {
+                price += 70;
+            } else {
+                price += 120;
+            }
+
+        }
+
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(!takeawayAdded){
+            takeawayAdded = true;
+            price += 20;
+        }
     }
 
     public String getBill(){
         // your code goes here
+        int toppings;
+        if(isVeg){
+            toppings = 70;
+        }
+        else{
+            toppings = 120;
+        }
+        StringBuilder str = new StringBuilder("");
+        str.append("Base Price for the Pizza is: "+ basePrice +'\n');
+        if(cheeseAdded){
+            str.append("Extra Cheese is Added: "+80+'\n');
+        }
+        if(toppingsAdded){
+            str.append("Extra Topping is Added: "+toppings+'\n');
+        }
+        if(takeawayAdded){
+            str.append("Paper bag is Added: "+20+'\n');
+        }
+        str.append("Total Price is: "+price+'\n');
+        bill = str.toString();
         return this.bill;
     }
 }
